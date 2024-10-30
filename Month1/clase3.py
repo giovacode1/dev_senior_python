5b5ea9849673fb54ac36e1a99969702934b6f1c0
giovannicode <116524974+giovacode1@users.noreply.github.com>
2024-10-29 21:43:27 -0500

    Create clase3.py

---
+++ Month1/clase3.py
@@ -0,0 +1,19 @@
+#* importacion de librerias 
+import math 
+#la funcion mas usada para importar librerias es import 
+resultado = math.sqrt(23)
+print(resultado)
+#estructuras de control , condicionales , if , else , elif
+##! como se usan , permiten ejecutar diferentes bloques de codigo segun ciertas condiciones. es como decirtlo a tu programa si ocurre algo haz esto 
+edad = 18 
+if edad >= 18: 
+    print("eres mayor de edad ")
+elif edad >= 13: 
+    print("eres un adolecente")
+else: 
+    print("eres un niño ")
+
+for i in range(0,200,10):
+    print(i)
+#cero inicia , segundo es el limite , tercero el numero de conteos 
+ 
