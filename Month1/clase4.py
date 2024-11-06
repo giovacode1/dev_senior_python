5cae5f87378971792a71838f48dc4be5f9ca7c8a
giovannicode <116524974+giovacode1@users.noreply.github.com>
2024-11-05 21:22:09 -0500

    ok

---
+++ Month1/clase4.py
@@ -0,0 +1,12 @@
+#gestion de la drogueria
+#! En este archivo vamos a crear un sistema de gestion de ventas para una drogueria 
+#* Vamos a trabajar las listas 
+#! codoficacion camel case 
+# 1.7 float , numeros con decimales , int 12 , numeros sin decimales , str numeros con letras "giovanni"
+
+ventasTotales = 0.0 
+#! listas para almacenar la informacion 
+nombres = []
+precios = []
+cantidades = []
+
